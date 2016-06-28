992dfc1014d0b223f25f41ffc749570e0549519e
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2016-06-28 10:37:27 +0200

    handle lang

--- SwitchToClassicGoogle.js
+++ SwitchToClassicGoogle.js
@@ -2,7 +2,8 @@
   var loc = window.location,
     href = loc.href,
     query = href.replace(/^.*[#?&]q=([^#?&]+).*$/, '$1'),
-    total = (~href.search(/num=/) ? href.replace(/^.*[#?&]num=(\d+).*$/, '$1') : 100),
+    hlang = (~href.search(/hl=/) ? href.replace(/^.*[#?&](hl=[^#?&]+).*$/, '&$1') : '&hl=fr'),
+    total = (~href.search(/num=/) ? href.replace(/^.*[#?&](num=\d+).*$/, '&$1') : '&num=100'),
     start = (~href.search(/start=/) ? href.replace(/^.*[#?&](start=\d+).*$/, '&$1') : '');
-  href = loc.protocol + "//" + loc.hostname + "/search?q=" + query + "&num=" + total + start;
+  window.location.href = loc.protocol + "//" + loc.hostname + "/search?q=" + query + hlang + total + start;
 })();
