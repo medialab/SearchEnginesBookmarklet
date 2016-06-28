4e0c0cd11a4382dfa81662759c36d5869f74eed0
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2016-06-28 10:44:09 +0200

    remain on classic google when changing page

--- extract_google_results.js
+++ extract_google_results.js
@@ -1,13 +1,15 @@
 (function(){
-  var href = window.location.href,
+  var loc = window.location,
+    href = loc.href,
     query = href.replace(/^.*[#?&]q=([^#?&]+).*$/, '$1'),
+    hlang = (~href.search(/hl=/) ? href.replace(/^.*[#?&](hl=[^#?&]+).*$/, '&$1') : '&hl=fr'),
     total = (~href.search(/num=/) ? href.replace(/^.*[#?&]num=(\d+).*$/, '$1') : 100),
     start = (~href.search(/start=/) ? href.replace(/^.*[#?&]start=(\d+).*$/, '$1') : 0),
     page = start/total,
     data = artoo.scrape("h3.r a", {"url": "href", "name": "text"});
   artoo.log.verbose(query, start, total, page, data.length);
   artoo.saveCsv(data, "google-results-"+query+"-"+page+".csv");
-  if (window.confirm("CSV downloaded with "+total+" results from page "+page+" of Google results for query "+ query + "\nDo you want to go to the next page?")) {
-    artoo.$(".b.navend a")[0].click()
+  if (window.confirm("CSV downloaded with "+data.length+" results from page "+page+" of Google results for query "+ query + "\nDo you want to go to the next page?")) {
+    window.location.href = loc.protocol + "//" + loc.hostname + "/search?q=" + query + hlang + "&num=" + total + "&start=" + (start + num);
   }
 })();
