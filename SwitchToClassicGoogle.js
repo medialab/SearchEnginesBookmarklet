1e047599c8433c96a893677e15286fad6faaa406
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2016-06-28 10:26:16 +0200

    cleanup

--- SwitchToClassicGoogle.js
+++ SwitchToClassicGoogle.js
@@ -1,7 +1,8 @@
 (function(){
-  var href = window.location.href,
+  var loc = window.location,
+    href = loc.href,
     query = href.replace(/^.*[#?&]q=([^#?&]+).*$/, '$1'),
     total = (~href.search(/num=/) ? href.replace(/^.*[#?&]num=(\d+).*$/, '$1') : 100),
     start = (~href.search(/start=/) ? href.replace(/^.*[#?&](start=\d+).*$/, '&$1') : '');
-  window.location.href = window.location.protocol + "//" + window.location.hostname + "/search?q=" + query + "&num=100" + start;
+  href = loc.protocol + "//" + loc.hostname + "/search?q=" + query + "&num=" + total + start;
 })();
