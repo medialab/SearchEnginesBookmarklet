256a7342d919840b8ba6139f76d28dc64c34f1d8
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2016-06-28 10:51:02 +0200

    use integers

--- extract_google_results.js
+++ extract_google_results.js
@@ -3,8 +3,8 @@
     href = loc.href,
     query = href.replace(/^.*[#?&]q=([^#?&]+).*$/, '$1'),
     hlang = (~href.search(/hl=/) ? href.replace(/^.*[#?&](hl=[^#?&]+).*$/, '&$1') : '&hl=fr'),
-    total = (~href.search(/num=/) ? href.replace(/^.*[#?&]num=(\d+).*$/, '$1') : 100),
-    start = (~href.search(/start=/) ? href.replace(/^.*[#?&]start=(\d+).*$/, '$1') : 0),
+    total = (~href.search(/num=/) ? parseInt(href.replace(/^.*[#?&]num=(\d+).*$/, '$1')) : 100),
+    start = (~href.search(/start=/) ? parseInt(href.replace(/^.*[#?&]start=(\d+).*$/, '$1')) : 0),
     page = start/total,
     data = artoo.scrape("h3.r a", {"url": "href", "name": "text"});
   artoo.log.verbose(query, start, total, page, data.length);
