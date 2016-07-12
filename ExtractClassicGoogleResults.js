9e4262c3a75b5f9438eac632cc717dc9e493c2a8
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2016-07-12 16:30:46 +0200

    I had a dream of @Yomguithereal fixing artoo

--- ExtractClassicGoogleResults.js
+++ ExtractClassicGoogleResults.js
@@ -3,16 +3,19 @@
     //log: {beeping: true},
     //scriptUrl: "//localhost:4443/extract_google_results.js" // debug
     scriptUrl: "//medialab.github.io/google-bookmarklets/extract_google_results.js"
+  }, injectScript = function(body, url, name, settings){
+    var a = document.createElement("script");
+    console.log("Loading "+name+".js...");
+    a.src = url;
+    a.type = "text/javascript";
+    a.id = name + "_injected_script";
+    if (settings) a.setAttribute("settings", JSON.stringify(settings));
+    body.appendChild(a);
   }, e = !0;
   if ("object" == typeof this.artoo && (artoo.settings.reload || (artoo.loadSettings(t), artoo.exec(), e = !1)), e){
     var bod = document.getElementsByTagName("body")[0];
     bod || (bod = document.createElement("body"), document.documentElement.appendChild(bod));
-    var a = document.createElement("script");
-    console.log("Loading artoo.js...");
-    a.src = "//medialab.github.io/artoo/public/dist/artoo-latest.min.js";
-    a.type = "text/javascript";
-    a.id = "artoo_injected_script";
-    a.setAttribute("settings", JSON.stringify(t));
-    bod.appendChild(a);
+    injectScript(bod, "//medialab.github.io/artoo/public/dist/artoo-latest.min.js", "artoo", t);
+    injectScript(bod, "//cdn.rawgit.com/eligrey/FileSaver.js/e9d941381475b5df8b7d7691013401e171014e89/FileSaver.min.js", "FileSaver");
   }
 }).call(this);
