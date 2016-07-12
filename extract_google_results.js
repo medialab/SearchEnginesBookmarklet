e25ed96312d059b5d3fe8cf7a9478fe3b808915e
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2016-07-12 18:23:36 +0200

    add ui to select number of results and lang in redirecter

--- extract_google_results.js
+++ extract_google_results.js
@@ -6,12 +6,12 @@
     total = (~href.search(/num=/) ? parseInt(href.replace(/^.*[#?&]num=(\d+).*$/, '$1')) : 100),
     start = (~href.search(/start=/) ? parseInt(href.replace(/^.*[#?&]start=(\d+).*$/, '$1')) : 0),
     page = start/total,
-    data = artoo.scrape("h3.r a", {"url": "href", "name": "text"});
+    data = artoo.scrape("h3.r a", {"url": "href", "name": "text"}),
+    blob = new Blob([artoo.writers.csv(data)], {type: "text/plain;charset=utf-8"});
   artoo.log.verbose(data.length + " results for query '" + query + "' on page " + page);
-  var blob = new Blob([artoo.writers.csv(data)], {type: "text/plain;charset=utf-8"});
-  saveAs(blob, "google-results-"+query+"-"+page+".csv");
+  saveAs(blob, "google-results-" + query + "-" + page + ".csv");
   setTimeout(function(){
-    if (window.confirm("CSV downloaded with "+data.length+" results from page "+page+" of Google results for query "+ query + "\nDo you want to go to the next page?")) {
+    if (window.confirm("CSV downloaded with "+data.length+" results from page "+page+" of Google results for query " + query + "\nDo you want to go to the next page?")) {
       window.location.href = loc.protocol + "//" + loc.hostname + "/search?q=" + query + hlang + "&num=" + total + "&start=" + (start + total);
     }
   }, 350);
--- switch_classic_google.js
+++ switch_classic_google.js
@@ -2,12 +2,47 @@
   var loc = window.location,
     href = loc.href,
     query = (~href.search(/[#?&]q=/) ? href.replace(/^.*[#?&]q=([^#?&]+).*$/, '$1') : undefined),
-    hlang = (~href.search(/hl=/) ? href.replace(/^.*[#?&](hl=[^#?&]+).*$/, '&$1') : '&hl=fr'),
-    total = (~href.search(/num=/) ? href.replace(/^.*[#?&](num=\d+).*$/, '&$1') : '&num=100'),
-    start = (~href.search(/start=/) ? href.replace(/^.*[#?&](start=\d+).*$/, '&$1') : '');
-  if (!~window.location.href.search(/:\/\/([^.]+\.)?google\.[^/]+\//))
-    return window.alert("You can only use this bookmarklet on Google websites.");
-  else if (!query)
-    return window.alert("Please search for a keyword first.");
-  window.location.href = loc.protocol + "//" + loc.hostname + "/search?q=" + query + hlang + total + start;
+    hlang = (~href.search(/hl=/) ? href.replace(/^.*[#?&]hl=([^#?&]+).*$/, '$1') : 'fr'),
+    total = (~href.search(/num=/) ? parseInt(href.replace(/^.*[#?&]num=(\d+).*$/, '$1')) : 100),
+    languages = ['en', 'fr', 'es', 'de'],
+    results = [10, 20, 50, 100],
+    styles = [
+      '#overlay {z-index: 1000000; position: fixed; top: 150px; right: 10px; background-color: white; height: 250px; width: 330px; border-radius: 5px; box-shadow: 1px 1px 5px 3px #656565; padding: 20px; text-align: center;}',
+      'h2 {margin: 0px 0px 30px 0px; text-decoration: underline;}',
+      'select {margin-left: 10px;}',
+      '#url {width: 90%;}'
+    ],
+    ui = new artoo.ui();
+  ui.$().html('<style>' + styles.join('\n') + '</style>' +
+              '<div id="overlay">' +
+                '<h2>Redirect to Classic Google</h2>' +
+                '<p>Which language?' +
+                '<select id="lang"></select></p>' +
+                '<p>How many results per page?' +
+                '<select id="results"></select></p>' +
+                '<p>You will be redirected to the following url:</p>' +
+                '<textarea id="url" disabled="disabled" rows="3" />' +
+                '<br/>' +
+                '<input id="redirect" type="button" value="Redirect me!"></input>' +
+              '</div>');
+    ui.populateSelect = function(selector, values, curVal){
+      if (!~values.indexOf(curVal)) values.unshift(curVal);
+      values.forEach(function(v) {
+        var option = document.createElement('option');
+        option.setAttribute('value', v);
+        option.innerHTML = v;
+        if (curVal === v) option.selected = 'selected';
+        ui.shadow.querySelector(selector).appendChild(option);
+      });
+    };
+    ui.populateSelect('#lang', languages, hlang);
+    ui.populateSelect('#results', results, total);
+    ui.buildUrl = function(){
+      ui.$('#url').val(loc.protocol + "//" + loc.hostname + "/search?q=" + query + '&hl=' + ui.$('#lang').val() + '&num=' + ui.$('#results').val() + '&start=0');
+    };
+    ui.buildUrl();
+    ui.$('#lang, #results').on('change', ui.buildUrl);
+    ui.$("#redirect").on('click', function(){
+      window.location.href = ui.$('#url').val();
+    })
 })();
