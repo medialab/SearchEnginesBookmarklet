5c22548c897165f019f7bc6d11172b236c65bb69
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2016-07-12 15:01:23 +0200

    artoo dl fixed for firefox, reverting use of filesaver.js

--- ExtractClassicGoogleResults.js
+++ ExtractClassicGoogleResults.js
@@ -1,9 +1,10 @@
 (function(){
   var t = {
     //log: {beeping: true},
+    //scriptUrl: "//localhost:4443/extract_google_results.js" // debug
     scriptUrl: "//medialab.github.io/google-bookmarklets/extract_google_results.js"
   }, e = !0;
-  if ("object" == typeof this.artoo && (artoo.settings.reload || (artoo.loadSettings(t), artoo.exec(), artoo.beep(), e = !1)), e){
+  if ("object" == typeof this.artoo && (artoo.settings.reload || (artoo.loadSettings(t), artoo.exec(), e = !1)), e){
     var bod = document.getElementsByTagName("body")[0];
     bod || (bod = document.createElement("body"), document.documentElement.appendChild(bod));
     var a = document.createElement("script");
@@ -13,12 +14,5 @@
     a.id = "artoo_injected_script";
     a.setAttribute("settings", JSON.stringify(t));
     bod.appendChild(a);
-    var b = document.createElement("script");
-    console.log("Loading FileSaver.js...");
-    b.src = "//cdn.rawgit.com/eligrey/FileSaver.js/e9d941381475b5df8b7d7691013401e171014e89/FileSaver.min.js";
-    b.type = "text/javascript";
-    b.id = "filesaver_injected_script";
-    b.setAttribute("settings", JSON.stringify(t));
-    bod.appendChild(b);
   }
 }).call(this);
--- extract_google_results.js
+++ extract_google_results.js
@@ -7,9 +7,11 @@
     start = (~href.search(/start=/) ? parseInt(href.replace(/^.*[#?&]start=(\d+).*$/, '$1')) : 0),
     page = start/total,
     data = artoo.scrape("h3.r a", {"url": "href", "name": "text"});
-  var blob = new Blob([artoo.writers.csv(data)], {type: "text/plain;charset=utf-8"});
-  saveAs(blob, "google-results-"+query+"-"+page+".csv");
-  if (window.confirm("CSV downloaded with "+data.length+" results from page "+page+" of Google results for query "+ query + "\nDo you want to go to the next page?")) {
-    window.location.href = loc.protocol + "//" + loc.hostname + "/search?q=" + query + hlang + "&num=" + total + "&start=" + (start + total);
-  }
+  artoo.log.verbose(data.length + " results for query '" + query + "' on page " + page);
+  artoo.saveCsv(data, "google-results-"+query+"-"+page+".csv");
+  setTimeout(function(){
+    if (window.confirm("CSV downloaded with "+data.length+" results from page "+page+" of Google results for query "+ query + "\nDo you want to go to the next page?")) {
+      window.location.href = loc.protocol + "//" + loc.hostname + "/search?q=" + query + hlang + "&num=" + total + "&start=" + (start + total);
+    }
+  }, 200);
 })();
