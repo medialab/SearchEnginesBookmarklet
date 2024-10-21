d4e2096b6186972d2dec8ed219ab0f8d5dc10f00
Julien Pontoire <pontoirejulien@gmail.com>
2024-10-21 17:50:51 +0200

    Update Baidu extract popup

--- extract_baidu_results.js
+++ extract_baidu_results.js
@@ -24,24 +24,22 @@
             '#BBMoverlay {z-index: 1000000; position: fixed; top: 150px; right: 10px; background-color: white; height: 250px; width: 330px; border-radius: 5px; box-shadow: 1px 1px 5px 3px #656565; padding: 20px; text-align: center;}',
             '#BBMoverlay h2 {margin: 0px 0px 15px 0px; text-decoration: underline;}',
             '#BBMoverlay input[type="button"] {margin: 3px;}'
-          ];
+          ],
+          href = window.location.href,
+          total = (~href.search(/rn=/) ? parseInt(href.replace(/^.*[#?&]rn=(\d+).*$/, '$1')) : 50),
+          start = (~href.search(/pn=/) ? parseInt(href.replace(/^.*[#?&]pn=(\d+).*$/, '$1')) : 0),
+          query = (~href.search(/[#?&]wd=/) ? href.replace(/^.*[#?&]wd=([^#?&]+).*$/, '$1') : undefined),
+          page = start/total;
         
         artoo.$('body').append('<style>' + styles.join('\n') + '</style>' +
         '<div id="BBMoverlay">' +
         '<h2>Extract Baidu Results</h2>' +
-        '<select id="BBM_number">' +
-        '<option value="10">10</option>' +
-        '<option value="20">20</option>' +
-        '<option value="50">50</option>' +
-        '<option value="100">100</option>' +
-        '<option value="1000">1000</option>' +
-        '</select>' +
+        '<p>Search for "<b>' + decodeURIComponent(query.replace(/\+/g, '%20')) + '</b>"<br/>' +
+        'page ' + page + ' (with up to ' + total + ' urls per page)</p>' +
         '<input class="BBMdownload" type="button" value="Begin scraping"></input><br/>' +
         '</div>');
 
         artoo.$("#BBMoverlay .BBMdownload").on('click', async function(){
-            select = document.querySelector('select[id="BBM_number"]')
-            const n = parseInt(select.value, 10);
             const data = await scrape();
             artoo.saveCsv(data);
         });
