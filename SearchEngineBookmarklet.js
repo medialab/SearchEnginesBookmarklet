9266a0b53a9c282b4bc41a92dc88114c776234e2
Julien Pontoire <pontoirejulien@gmail.com>
2024-10-25 10:26:55 +0200

    Fixing script not working for bing

--- SearchEngineBookmarklet.js
+++ SearchEngineBookmarklet.js
@@ -1,25 +1,25 @@
 (function(){
   var t = {
-    // scriptUrl: "//localhost:4443/switch_classic_google.js" // for debug using `node serve-https.js`
-    scriptUrl: "//medialab.github.io/google-bookmarklets/switch_classic_google.js"
+    scriptUrl: "//localhost:4443/switch_classic_google.js" // for debug using `node serve-https.js`
+    // scriptUrl: "//medialab.github.io/google-bookmarklets/switch_classic_google.js"
   }, s = {
-    // scriptUrl: "//localhost:4443/extract_google_results.js" // for debug using `node serve-https.js`
-    scriptUrl: "//medialab.github.io/google-bookmarklets/extract_google_results.js"
+    scriptUrl: "//localhost:4443/extract_google_results.js" // for debug using `node serve-https.js`
+    // scriptUrl: "//medialab.github.io/google-bookmarklets/extract_google_results.js"
   }, d = {
-    // scriptUrl: "//localhost:4443/extract_duckduckgo_results.js" // for debug using `node serve-https.js`
-    scriptUrl: "//medialab.github.io/google-bookmarklets/extract_duckduckgo_results.js"
+    scriptUrl: "//localhost:4443/extract_duckduckgo_results.js" // for debug using `node serve-https.js`
+    // scriptUrl: "//medialab.github.io/google-bookmarklets/extract_duckduckgo_results.js"
   }, b = {
-    // scriptUrl: "//localhost:4443/extract_baidu_results.js" // for debug using `node serve-https.js`
-    scriptUrl: "//medialab.github.io/google-bookmarklets/extract_baidu_results.js"
+    scriptUrl: "//localhost:4443/extract_baidu_results.js" // for debug using `node serve-https.js`
+    // scriptUrl: "//medialab.github.io/google-bookmarklets/extract_baidu_results.js"
   }, m = {
-    // scriptUrl: "//localhost:4443/switch_more_baidu.js" // for debug using `node serve-https.js`
-    scriptUrl: "//medialab.github.io/google-bookmarklets/switch_more_baidu.js"
+    scriptUrl: "//localhost:4443/switch_more_baidu.js" // for debug using `node serve-https.js`
+    // scriptUrl: "//medialab.github.io/google-bookmarklets/switch_more_baidu.js"
   }, q = {
-    // scriptUrl: "//localhost:4443/extract_qwant_results.js" // for debug using `node serve-https.js`
-    scriptUrl: "//medialab.github.io/google-bookmarklets/extract_qwant_results.js"
+    scriptUrl: "//localhost:4443/extract_qwant_results.js" // for debug using `node serve-https.js`
+    // scriptUrl: "//medialab.github.io/google-bookmarklets/extract_qwant_results.js"
   }, o = {
-    // scriptUrl: "//localhost:4443/switch_more_bing.js" // for debug using `node serve-https.js`
-    scriptUrl: "//medialab.github.io/google-bookmarklets/switch_more_bing.js"
+    scriptUrl: "//localhost:4443/switch_more_bing.js" // for debug using `node serve-https.js`
+    // scriptUrl: "//medialab.github.io/google-bookmarklets/switch_more_bing.js"
   }, injectScript = function(body, url, name, settings){
     var a = document.createElement("script");
     console.log("Loading "+name+".js...");
@@ -89,7 +89,6 @@
     }
   }
   else if(~href.search(/:\/\/([^.]+\.)?bing\.[^/]+\//)){
-    var bingOverride = HTMLBodyElement.prototype.appendChild;
     HTMLBodyElement.prototype.appendChild = Node.prototype.appendChild;
     const query = (~href.search(/[#?&]q=/) ? href.replace(/^.*[#?&]q=([^#?&]+).*$/, '$1') : undefined);
     if(!query)
@@ -99,7 +98,6 @@
         bod || (bod = document.createElement("body"), document.documentElement.appendChild(bod));
         injectScript(bod, "//medialab.github.io/artoo/public/dist/artoo-latest.min.js", "artoo", o);
       }
-    HTMLBodyElement.prototype.appendChild = bingOverride;
   }
   else
     return window.alert("You can only use this bookmarklet on Google, DuckDuckGo, Qwant and Baidu websites.");
