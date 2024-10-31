cfe2e1cc3dbc4fd1d898c8f58772843beeb1bfd0
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2024-10-31 19:01:55 +0100

    refacto

--- SearchEnginesBookmarklet.js
+++ SearchEnginesBookmarklet.js
@@ -1,98 +1,75 @@
 (function(){
   const scriptsDomain = "//medialab.github.io/google-bookmarklets/";
   // const scriptsDomain = "//localhost:4443/"; // for debug using `node serve-https.js`
-  const more = {
-    scriptUrl: scriptsDomain + "switch_more_results.js"
-  }, autoscroll = {
-    scriptUrl: scriptsDomain + "extract_autoscroll_results.js"
-  }, pagination = {
-    scriptUrl: scriptsDomain + "extract_pagination_results.js"
-  }, injectScript = function(body, url, name, settings){
-    var a = document.createElement("script");
-    console.log("Loading "+name+".js...");
-    a.src = url;
-    a.type = "text/javascript";
-    a.id = name + "_injected_script";
-    if (settings) a.setAttribute("settings", JSON.stringify(settings));
-    body.appendChild(a);
-  }, href = window.location.href;
+
+  const moreResults = "switch_more_results.js",
+    pagination = "extract_pagination_results.js",
+    autoscroll = "extract_autoscroll_results.js";
+
+  const href = window.location.href;
   var e = !0;
+
+  const injectScriptWithArtoo = function(body, script){
+    var a = document.createElement("script");
+    console.log("Loading artoo.js...");
+    a.src = "//medialab.github.io/artoo/public/dist/artoo-latest.min.js";
+    a.type = "text/javascript";
+    a.id = "artoo_injected_script";
+    if (script)
+      a.setAttribute("settings", JSON.stringify({scriptUrl: scriptsDomain + script}));
+    body.appendChild(a);
+  };
+
+  const addPaginatedScraper = function(query, nResultsArg, startArg){
+    if(!query)
+      return window.alert("Please search for a keyword first.");
+    if ("object" == typeof this.artoo && (artoo.settings.reload || (artoo.loadSettings({scriptUrl: scriptsDomain + pagination}), artoo.exec(), e = !1)), e){
+      var bod = document.getElementsByTagName("body")[0];
+      bod || (bod = document.createElement("body"), document.documentElement.appendChild(bod));
+      injectScriptWithArtoo(bod, href.includes(nResultsArg + "=") && href.includes(startArg + "=") ? pagination : moreResults);
+    }
+  };
+
+  const addAutoscrollScraper = function(query){
+    if (!query)
+      return window.alert("Please search for a keyword first.");
+    if ("object" == typeof this.artoo && (artoo.settings.reload || (artoo.loadSettings({scriptUrl: scriptsDomain + autoscroll}), artoo.exec(), e = !1)), e){
+      var bod = document.getElementsByTagName("body")[0];
+      bod || (bod = document.createElement("body"), document.documentElement.appendChild(bod));
+      injectScriptWithArtoo(bod, autoscroll);
+    }
+  };
 
   // Google
   if(~href.search(/:\/\/([^.]+\.)?google\.[^/]+\//)){
     const query = (~href.search(/[#?&]q=/) ? href.replace(/^.*[#?&]q=([^#?&]+).*$/, '$1') : undefined);
-    if(!query)
-      return window.alert("Please search for a keyword first.");
-    if ("object" == typeof this.artoo && (artoo.settings.reload || (artoo.loadSettings(pagination), artoo.exec(), e = !1)), e){
-      var bod = document.getElementsByTagName("body")[0];
-      bod || (bod = document.createElement("body"), document.documentElement.appendChild(bod));
-      injectScript(
-        bod,
-        "//medialab.github.io/artoo/public/dist/artoo-latest.min.js",
-        "artoo",
-        href.includes("num=") && href.includes("start=") ? pagination : more
-      );
-    }
+    addPaginatedScraper(query, "num", "start");
   }
 
   // DuckDuckGo
   else if(~href.search(/:\/\/([^.]+\.)?duckduckgo\.[^/]+\//)){
     const query = (~href.search(/[#?&]q=/) ? href.replace(/^.*[#?&]q=([^#?&]+).*$/, '$1') : undefined);
-    if (!query)
-      return window.alert("Please search for a keyword first.");
-    if ("object" == typeof this.artoo && (artoo.settings.reload || (artoo.loadSettings(autoscroll), artoo.exec(), e = !1)), e){
-      var bod = document.getElementsByTagName("body")[0];
-      bod || (bod = document.createElement("body"), document.documentElement.appendChild(bod));
-      injectScript(bod, "//medialab.github.io/artoo/public/dist/artoo-latest.min.js", "artoo", autoscroll);
-    }
+    addAutoscrollScraper(query);
   }
 
   // Baidu
   else if(~href.search(/:\/\/([^.]+\.)?baidu\.[^/]+\//)){
     const query = (~href.search(/[#?&]wd=/) ? href.replace(/^.*[#?&]wd=([^#?&]+).*$/, '$1') : undefined);
-    if(!query)
-      return window.alert("Please search for a keyword first."); 
-    if ("object" == typeof this.artoo && (artoo.settings.reload || (artoo.loadSettings(pagination), artoo.exec(), e = !1)), e){
-      var bod = document.getElementsByTagName("body")[0];
-      bod || (bod = document.createElement("body"), document.documentElement.appendChild(bod));
-      injectScript(
-        bod,
-        "//medialab.github.io/artoo/public/dist/artoo-latest.min.js",
-        "artoo",
-        href.includes("rn=") && href.includes("pn=") ? pagination : more
-      );
-    }
+    addPaginatedScraper(query, "rn", "pn");
   }
 
   // Qwant
   else if(~href.search(/:\/\/([^.]+\.)?qwant\.[^/]+\//)){
     const query = (~href.search(/[#?&]q=/) ? href.replace(/^.*[#?&]q=([^#?&]+).*$/, '$1') : undefined);
-    if (!query)
-      return window.alert("Please search for a keyword first.");
-    if ("object" == typeof this.artoo && (artoo.settings.reload || (artoo.loadSettings(autoscroll), artoo.exec(), e = !1)), e){
-      var bod = document.getElementsByTagName("body")[0];
-      bod || (bod = document.createElement("body"), document.documentElement.appendChild(bod));
-      injectScript(bod, "//medialab.github.io/artoo/public/dist/artoo-latest.min.js", "artoo", autoscroll);
-    }
+    addAutoscrollScraper(query);
   }
 
   // Bing
   else if(~href.search(/:\/\/([^.]+\.)?bing\.[^/]+\//)){
     HTMLBodyElement.prototype.appendChild = Node.prototype.appendChild;
     const query = (~href.search(/[#?&]q=/) ? href.replace(/^.*[#?&]q=([^#?&]+).*$/, '$1') : undefined);
-    if(!query)
-      return window.alert("Please search for a keyword first."); 
-    if ("object" == typeof this.artoo && (artoo.settings.reload || (artoo.loadSettings(pagination), artoo.exec(), e = !1)), e){
-      var bod = document.getElementsByTagName("body")[0];
-      bod || (bod = document.createElement("body"), document.documentElement.appendChild(bod));
-      injectScript(
-        bod,
-        "//medialab.github.io/artoo/public/dist/artoo-latest.min.js",
-        "artoo",
-        href.includes("q=") && href.includes("first=") ? pagination : more
-      );
-    }
+    addPaginatedScraper(query, "q", "first");
   }
 
-  else return window.alert("You can only use this bookmarklet on Google, DuckDuckGo, Qwant, Bing and Baidu websites.");
+  else return window.alert("You can only use this bookmarklet on Google, DuckDuckGo, Baidu, Qwant and Bing.");
 }).call(this);
