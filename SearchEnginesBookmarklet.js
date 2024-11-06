34791b3e3f7958d96a877613e020eb6cb6cfcb0f
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2024-11-06 17:15:07 +0100

    ensure bookmarklet only loads once in a page + code readability

--- SearchEnginesBookmarklet.js
+++ SearchEnginesBookmarklet.js
@@ -2,7 +2,9 @@
   const scriptsDomain = "//medialab.github.io/SearchEnginesBookmarklet/";
   // const scriptsDomain = "//localhost:4443/"; // for debug using `node serve-https.js`
 
-  if ("undefined" == typeof window.SearchEnginesBookmarklet) {
+  if (window.artoo !== undefined && document.getElementById("BMoverlay")) return;
+
+  if (window.SearchEnginesBookmarklet === undefined) {
     window.alert("It seems you are using an old version of this tool.\n\nThe new version works with more search engines such as DuckDuckGo, Baidu, Bing and Qwant.\n\nPlease remove it from your bookmarks and reinstall it by visiting the page that will open in a new tab (first allow it to pop-up).");
     window.open("https://medialab.github.io/SearchEnginesBookmarklet/");
   }
@@ -12,10 +14,20 @@
     autoscroll = "extract_autoscroll_results.js";
 
   const href = window.location.href;
-  var e = !0;
 
-  const injectScriptWithArtoo = function(body, script){
-    if ("object" != typeof body) return;
+  const injectScriptWithArtoo = function(queryArg, script){
+    const searchRegexp = new RegExp("[#?&]" + queryArg + "="),
+      replaceRegexp = new RegExp("^.*[#?&]" + queryArg + "=([^#?&]+).*$"),
+      query = (~href.search(searchRegexp) ? href.replace(replaceRegexp, '$1') : undefined);
+    if (!query) return window.alert("Please input your search query first.");
+
+    var body = document.getElementsByTagName("body")[0];
+    if (!body) {
+      body = document.createElement("body");
+      document.documentElement.appendChild(body);
+    }
+    if (typeof body !== "object") return;
+
     var a = document.createElement("script");
     console.log("Loading artoo.js...");
     a.src = "//medialab.github.io/artoo/public/dist/artoo-latest.min.js";
@@ -25,54 +37,30 @@
     body.appendChild(a);
   };
 
-  const checkQuery = function(queryArg){
-    const searchRegexp = new RegExp("[#?&]" + queryArg + "="),
-      replaceRegexp = new RegExp("^.*[#?&]" + queryArg + "=([^#?&]+).*$"),
-      query = (~href.search(searchRegexp) ? href.replace(replaceRegexp, '$1') : undefined);
-    if (!query) return window.alert("Please input your search query first.");
-    var bod = document.getElementsByTagName("body")[0];
-    bod || (bod = document.createElement("body"), document.documentElement.appendChild(bod));
-    return bod;
-  }
-
-  const addPaginatedScraper = function(queryArg, nResultsArg, startArg){
-    const bod = checkQuery(queryArg);
-    if ("object" == typeof this.artoo && (artoo.settings.reload || (artoo.loadSettings({scriptUrl: scriptsDomain + pagination}), artoo.exec(), e = !1)), e){
-      injectScriptWithArtoo(bod, href.includes(nResultsArg + "=") && href.includes(startArg + "=") ? pagination : moreResults);
-    }
-  };
-
-  const addAutoscrollScraper = function(queryArg){
-    const bod = checkQuery(queryArg);
-    if ("object" == typeof this.artoo && (artoo.settings.reload || (artoo.loadSettings({scriptUrl: scriptsDomain + autoscroll}), artoo.exec(), e = !1)), e){
-      injectScriptWithArtoo(bod, autoscroll);
-    }
-  };
-
   // Google
   if(~href.search(/:\/\/([^.]+\.)?google\.[^/]+\//)){
-    addPaginatedScraper("q", "num", "start");
+    injectScriptWithArtoo("q", href.includes("num=") && href.includes("start=") ? pagination : moreResults);
   }
 
   // DuckDuckGo
   else if(~href.search(/:\/\/([^.]+\.)?duckduckgo\.[^/]+\//)){
-    addAutoscrollScraper("q");
+    injectScriptWithArtoo("q", autoscroll);
   }
 
   // Baidu
   else if(~href.search(/:\/\/([^.]+\.)?baidu\.[^/]+\//)){
-    addPaginatedScraper("wd", "rn", "pn");
+    injectScriptWithArtoo("wd", href.includes("rn=") && href.includes("pn=") ? pagination : moreResults);
   }
 
   // Qwant
   else if(~href.search(/:\/\/([^.]+\.)?qwant\.[^/]+\//)){
-    addAutoscrollScraper("q");
+    injectScriptWithArtoo("q", autoscroll);
   }
 
   // Bing
   else if(~href.search(/:\/\/([^.]+\.)?bing\.[^/]+\//)){
     HTMLBodyElement.prototype.appendChild = Node.prototype.appendChild;
-    addPaginatedScraper("q", "q", "first");
+    injectScriptWithArtoo("q", href.includes("q=") && href.includes("first=") ? pagination : moreResults);
   }
 
   else window.alert("You can only use this bookmarklet on Google, DuckDuckGo, Baidu, Qwant and Bing.");
